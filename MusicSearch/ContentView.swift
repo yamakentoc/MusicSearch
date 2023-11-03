8d85b9de3065251fc6c5a437a3e5e88584b14aec
yamakentoc <yamakentoc@gmail.com>
2023-11-03 14:52:52 +0900

    first commit

---
+++ MusicSearch/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MusicSearch
+//
+//  Created by 山口賢登 on 2023/11/03.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ MusicSearch/MusicSearchApp.swift
@@ -0,0 +1,17 @@
+//
+//  MusicSearchApp.swift
+//  MusicSearch
+//
+//  Created by 山口賢登 on 2023/11/03.
+//
+
+import SwiftUI
+
+@main
+struct MusicSearchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
